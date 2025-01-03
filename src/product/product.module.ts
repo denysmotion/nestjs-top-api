34c4e074cd5500ee3b1be4b8c39a0f2cd26ac11e
denysmotion <denysmotion@gmail.com>
2025-01-03 11:51:45 +0100

    fill models

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { ProductModel } from './product.model';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  controllers: [ProductController]
+  controllers: [ProductController],
+  imports: [
+      TypegooseModule.forFeature([
+        {
+          typegooseClass: ProductModel,
+          schemaOptions: {
+            collection: 'Product'
+          }
+        }
+      ])
+    ]
 })
 export class ProductModule {}
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop } from "@typegoose/typegoose";
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+
 export enum TopLevelCategory {
 	Courses,
 	Services,
@@ -5,23 +8,57 @@ export enum TopLevelCategory {
 	Products
 }
 
-export class TopPageModel {
-	_id: string;
-	firstCategory: TopLevelCategory;
-	secondCategroy: string;
+export class HhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSalary: number;
+
+	@prop()
+	middleSalary: number;
+
+	@prop()
+	seniorSalary: number;
+}
+
+export class TopPageAdvantege {
+	@prop()
 	title: string;
+
+	@prop()
+	description: string;
+}
+
+export interface TopPageModel extends Base { }
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopLevelCategory })
+	firstCategory: TopLevelCategory;
+
+	@prop()
+	secondCategroy: string;
+
+	@prop({ unique: true })
+	alias: string;
+
+	@prop()
+	title: string;
+
+	@prop()
 	category: string;
-	hh?: {
-		count: number;
-		juniorSalary: number;
-		middleSalary: number;
-		seniorSalary: number;
-	};
-	advanteges: {
-		title: string;
-		description: string;
-	}[];
+
+	@prop({ type: () => HhData })
+	hh?: HhData;
+
+	@prop({ type: () => [TopPageAdvantege] })
+	advanteges: TopPageAdvantege[];
+
+	@prop()
 	seoText: string;
+
+	@prop()
 	tagsTitle: string;
+
+	@prop({ type: () => [String] })
 	tags: string[];
 }
